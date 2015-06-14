4c8b548a0a9eb4c66cc927363ae0d2ba3e912ec4
kxbmap <kxbmap@gmail.com>
2015-06-14 11:17:25 +0900

    Rewrite to use stack again

--- src/main/scala/atc001/A/Main.scala
+++ src/main/scala/atc001/A/Main.scala
@@ -1,5 +1,6 @@
 package atc001.A
 
+import java.util
 import scala.io.StdIn
 
 object Main extends App {
@@ -7,34 +8,39 @@ object Main extends App {
   println(if (solve()) "Yes" else "No")
 
   def solve(): Boolean = {
+    type V = (Int, Int)
+
     val Array(h, w) = StdIn.readLine().split(" ").map(_.toInt)
     val grid = Array.fill(h)(StdIn.readLine())
 
     def isIn(x: Int, y: Int): Boolean = x >= 0 && x < w && y >= 0 && y < h
 
-    def dfs(s: (Int, Int)): Boolean = {
+    def dfs(s: V): Boolean = {
+      val stack = new util.ArrayDeque[V](h * w)
       val visited = Array.ofDim[Boolean](h, w)
 
-      def dfs0(x: Int, y: Int): Boolean = {
-        if (!isIn(x, y) || visited(y)(x)) false
+      def dfs0(): Boolean =
+        if (stack.isEmpty) false
         else {
-          grid(y)(x) match {
-            case 's' | '.' =>
-              visited(y)(x) = true
-              val next = List((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
-              next.exists {
-                case (nx, ny) => dfs0(nx, ny)
-              }
-
-            case 'g' => true
-            case _   => false
+          val (x, y) = stack.pop()
+          if (grid(y)(x) == 'g') true
+          else {
+            visited(y)(x) = true
+            for {
+              n@(nx, ny) <- Seq((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
+              if isIn(nx, ny) && !visited(ny)(nx) && grid(ny)(nx) != '#'
+            } {
+              stack.push(n)
+            }
+            dfs0()
           }
         }
-      }
-      dfs0(s._1, s._2)
+
+      stack.push(s)
+      dfs0()
     }
 
-    def find(c: Char, y: Int): (Int, Int) = grid(y).indexOf(c) match {
+    def find(c: Char, y: Int): V = grid(y).indexOf(c) match {
       case -1 => find(c, y + 1)
       case x  => (x, y)
     }
