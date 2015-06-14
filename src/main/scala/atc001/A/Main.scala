64bb25ee6ecc2a54f23b1c0c9bf3b08d6922bb8c
kxbmap <kxbmap@gmail.com>
2015-06-14 09:36:50 +0900

    Rewrite to simple recursive function

--- src/main/scala/atc001/A/Main.scala
+++ src/main/scala/atc001/A/Main.scala
@@ -1,7 +1,5 @@
 package atc001.A
 
-import java.util
-import scala.annotation.{switch, tailrec}
 import scala.io.StdIn
 
 object Main extends App {
@@ -9,50 +7,34 @@ object Main extends App {
   println(if (solve()) "Yes" else "No")
 
   def solve(): Boolean = {
-    type V = (Int, Int)
-
     val Array(h, w) = StdIn.readLine().split(" ").map(_.toInt)
     val grid = Array.fill(h)(StdIn.readLine().toCharArray)
 
-    def get(v: V): Char = {
-      val (x, y) = v
-      if (x >= 0 && x < w && y >= 0 && y < h) grid(y)(x) else '#'
-    }
+    def isIn(x: Int, y: Int): Boolean = x >= 0 && x < w && y >= 0 && y < h
 
-    def dfs(s: V): Boolean = {
-      val stack = new util.ArrayDeque[V](h * w)
+    def dfs(s: (Int, Int)): Boolean = {
+      val visited = Array.ofDim[Boolean](h, w)
 
-      @tailrec
-      def dfs0(): Boolean =
-        if (stack.isEmpty) false
+      def dfs0(x: Int, y: Int): Boolean = {
+        if (!isIn(x, y) || visited(y)(x)) false
         else {
-          val (x, y) = stack.pop()
-          val next = List((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
-
-          @tailrec
-          def search(ns: List[V]): Boolean = ns match {
-            case Nil     => false
-            case v :: vs =>
-              (get(v): @switch) match {
-                case 'g' => true
-                case '.' => stack.push(v); search(vs)
-                case _   => search(vs)
+          grid(y)(x) match {
+            case 's' | '.' =>
+              visited(y)(x) = true
+              val next = List((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
+              next.exists {
+                case (nx, ny) => dfs0(nx, ny)
               }
-          }
 
-          if (search(next)) true
-          else {
-            grid(y)(x) = 'V'
-            dfs0()
+            case 'g' => true
+            case _   => false
           }
         }
-
-      stack.push(s)
-      dfs0()
+      }
+      dfs0(s._1, s._2)
     }
 
-    @tailrec
-    def find(c: Char, x: Int, y: Int): V =
+    def find(c: Char, x: Int, y: Int): (Int, Int) =
       if (grid(y)(x) == c) (x, y)
       else if (x < w) find(c, x + 1, y)
       else find(c, 0, y + 1)
